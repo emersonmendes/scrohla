c8137257ea88bac5326e107b2f883731c6c5c56d
Emerson Mendes <emerson.emendes@gmail.com.br>
2020-03-04 01:29:10 -0300

    melhorias

--- targets/reclameaqui.js
+++ targets/reclameaqui.js
@@ -5,43 +5,23 @@ const target = {
     execute: collect
 };
 
-function collect(scrohla, sendResult) {
+async function collect(scrohla, sendResult) {
 
     const driver = scrohla.getDriver();
     const By = scrohla.getBy();
 
-    // teste inicio carregamento pagina
-    driver.manage().timeouts().pageLoadTimeout(30000);
-
-    scrohla.start();
+    await scrohla.start();
 
     let result = {};
 
-    driver.findElement(By.xpath("//*[contains(@class,'business-seal')]//p"))
-        .then(e => e.getText())
-        .then(t => result.qualidade = t);
+    const scoreElem = await driver.findElement(By.xpath("//span[contains(@class,'current-score')]"));
+    result.score = await scoreElem.getText();
 
-    const brandLogoXpath = "//*[@class='brand-logo']";
+    result.response = "Success!"
 
-    scrohla.isElementVisible(brandLogoXpath)
-        .then(isVisible => result.isBrandLogoVisible = isVisible);
-    scrohla.isElementNotVisible(brandLogoXpath)
-        .then(isNotVisible => result.isBrandLogoNotVisible = isNotVisible);
-    scrohla.isElementLocated(brandLogoXpath)
-        .then(isLocated => result.isBrandLogoLocated = isLocated);
+    await scrohla.takeScreenshot();
 
-    scrohla.isElementVisible(brandLogoXpath, 10)
-        .then(isVisible => result.isBrandLogoVisibleWith10ms = isVisible);
-    scrohla.isElementNotVisible(brandLogoXpath, 10)
-        .then(isNotVisible => result.isBrandLogoNotVisibleWith10ms = isNotVisible);
-    scrohla.isElementLocated(brandLogoXpath, 10)
-        .then(isLocated => result.isBrandLogoLocatedWith10ms = isLocated);
-
-    scrohla.flow(() => result.response = "Success!");
-
-    scrohla.takeScreenshot();
-
-    scrohla.flow(() => sendResult(result));
+    sendResult(result);
 
 }
 
